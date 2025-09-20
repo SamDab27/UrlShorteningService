070f6f2dee88f61730164805e3958e1a21d3225d
SamDab27 <samarthdabral1536@gmail.com>
2025-09-20 16:19:23 +0530

    final

---
+++ src/main/java/com/example/urlShorteningService/UrlShorteningApplication.java
@@ -0,0 +1,11 @@
+package com.example.urlShorteningService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorteningApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShorteningApplication.class, args);
+    }
+}
